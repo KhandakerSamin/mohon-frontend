72509dbfd1c30f829ab55b591a07963c221ce8e4
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2025-09-17 14:37:07 +0600

    Unnessary lines remove

--- src/app/x-tra/page.js
+++ src/app/x-tra/page.js
@@ -1,8 +1,5 @@
 import Footer from '@/components/Global/Footer'
-import FooterXtra from '@/components/Global/FooterXtra'
 import Navbar from '@/components/Global/Navbar'
-import NavbarXtra from '@/components/Global/NavbarXtra'
-import AllProjects from '@/components/XTraPage/AllProjects'
 import AllProjects2 from '@/components/XTraPage/AllProjects2'
 import React from 'react'
 
@@ -10,10 +7,7 @@ const page = () => {
   return (
     <div>
       <Navbar />
-     {/* <NavbarXtra /> */}
-     {/* <AllProjects /> */}
      <AllProjects2 />
-     {/* <FooterXtra /> */}
      <Footer />
     </div>
   )
